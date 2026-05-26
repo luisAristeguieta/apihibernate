651261535886393f5fd71470aa712276f73e772b
luisjimenezgx@gmai.com <luisjimenezgx@gmail.com>
2026-05-25 23:02:51 -0400

    chore: proyecto Spring Boot creado

---
+++ src/main/java/apihibernate/ApihibernateApplication.java
@@ -0,0 +1,13 @@
+package apihibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApihibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApihibernateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/apihibernate/ApihibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package apihibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApihibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
