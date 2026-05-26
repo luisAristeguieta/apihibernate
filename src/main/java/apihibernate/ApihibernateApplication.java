3e9a8e641113c94859ba23125a8fa6cb6e1718c0
luisjimenezgx@gmai.com <luisjimenezgx@gmail.com>
2026-05-26 01:07:44 -0400

    test: pruebas de endpoints en Postman

--- src/main/java/apihibernate/ApihibernateApplication.java
+++ src/main/java/apihibernate/ApihibernateApplication.java
@@ -2,8 +2,13 @@ package apihibernate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.krakdev")
+@EnableJpaRepositories(basePackages = "com.krakdev")
+@EntityScan(basePackages = "com.krakdev")
+
 public class ApihibernateApplication {
 
 	public static void main(String[] args) {
