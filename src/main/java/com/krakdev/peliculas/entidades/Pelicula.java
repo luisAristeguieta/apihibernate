0380cd1b95affc356b271d6acc349fe6d22fbaa1
luisjimenezgx@gmai.com <luisjimenezgx@gmail.com>
2026-05-26 00:14:37 -0400

    feat: entidad Pelicula creada

---
+++ src/main/java/com/krakdev/peliculas/entidades/Pelicula.java
@@ -0,0 +1,98 @@
+package com.krakdev.peliculas.entidades;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "peliculas")
+public class Pelicula {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false, length = 100)
+	private String nombre;
+
+	@Column(nullable = false, length = 100)
+	private String director;
+
+	@Column(nullable = false, length = 100)
+	private String genero;
+
+	@Column(nullable = false)
+	private int duracion;
+
+	@Column(nullable = false)
+	private boolean disponible;
+
+	public Pelicula() {
+	}
+
+	public Pelicula(String nombre, String director, String genero, int duracion, boolean disponible) {
+		this.nombre = nombre;
+		this.director = director;
+		this.genero = genero;
+		this.duracion = duracion;
+		this.disponible = disponible;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDirector() {
+		return director;
+	}
+
+	public void setDirector(String director) {
+		this.director = director;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+
+	public int getDuracion() {
+		return duracion;
+	}
+
+	public void setDuracion(int duracion) {
+		this.duracion = duracion;
+	}
+
+	public boolean isDisponible() {
+		return disponible;
+	}
+
+	public void setDisponible(boolean disponible) {
+		this.disponible = disponible;
+	}
+
+	@Override
+	public String toString() {
+		return "Pelicula [id=" + id + ", nombre=" + nombre + ", director=" + director + ", genero=" + genero
+				+ ", duracion=" + duracion + ", disponible=" + disponible + "]";
+	}
+
+}
